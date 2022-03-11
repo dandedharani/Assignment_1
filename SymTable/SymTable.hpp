fb57cf0bed2710d0060ebb939f3d20be699070f7
dandedharani <98050722+dandedharani@users.noreply.github.com>
2022-03-11 13:16:04 +0530

    Add files via upload

---
+++ SymTable/SymTable.hpp
@@ -0,0 +1,16 @@
+struct SymTable
+{
+	const char *pcKey;
+	const char *pvValue;
+	SymTable *n;
+	SymTable *oSymTable;
+};
+
+struct SymTable* SymTable_new();
+void SymTable_put (SymTable **oSymTable, const char *pcKey, const char *pvValue);
+int SymTable_getLength (SymTable *oSymTable);
+int SymTable_contains (SymTable *oSymTable, const char *new_key);
+void SymTable_get (SymTable *oSymTable, const char *new_key);
+void SymTable_replace (SymTable *oSymTable, const char *new_key, const char *new_value);
+void SymTable_remove (SymTable **oSymTable, const char *new_key);
+void print_SymTable (SymTable *oSymTable);
---
+++ SymTable/SymTable_testfile.cpp
@@ -0,0 +1,22 @@
+#include "SymTable_functions.hpp"
+
+int main()
+{
+	SymTable* SymTable_new();
+    struct SymTable *oSymTable = NULL;
+    SymTable_put (&oSymTable,"b","char");
+    SymTable_put (&oSymTable,"c","int");
+    SymTable_put (&oSymTable,"d","float");
+	print_SymTable (oSymTable);
+    int x = SymTable_getLength (oSymTable);
+    cout<<"Length of SymTable is "<<x<<endl;
+    int y = SymTable_contains (oSymTable,"c");
+    cout<<y<<endl;
+	SymTable_get (oSymTable,"b");
+	SymTable_replace (oSymTable,"d","double");
+	print_SymTable (oSymTable);
+	SymTable_remove (&oSymTable,"b");
+	cout<<"SymTable after removing the given binding"<<endl;
+	print_SymTable (oSymTable);
+	return 0;
+}
